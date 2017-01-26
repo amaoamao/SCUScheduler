8b4086f8d8de5f761ff58efc4c7342623368b262
amaoamao <scumchy@gmail.com>
2017-01-26 21:17:18 +0800

    ver 1.0

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 /**
  * Created by mao on 17-1-24.
  */
-// Your Client ID can be retrieved from your project in the Google
-// Developer Console, https://console.developers.google.com
 var CLIENT_ID = '399696677123-4en316frmevqe3elac4ddl95qfdvtgfs.apps.googleusercontent.com';
 
 var SCOPES = ["https://www.googleapis.com/auth/calendar"];
 var network_unavailable = setInterval("Materialize.toast('要翻墙哦', 5000)", 8000);
 var mSchedule = [];
+var classStartTimeWJ = ['', '08:00:00', '08:55:00', '10:00:00', '10:55:00', '14:00:00', '14:55:00', '15:50:00', '16:55:00', '17:50:00', '19:30:00', '20:25:00', '21:20:00'];
+var classEndTimeWJ = ['', '08:45:00', '09:40:00', '10:45:00', '11:40:00', '14:45:00', '15:40:00', '16:35:00', '17:40:00', '18:35:00', '20:15:00', '21:10:00', '22:05:00'];
+var classStartTimeJA = ['', '08:15:00', '09:10:00', '10:15:00', '11:10:00', '13:50:00', '14:45:00', '15:40:00', '16:45:00', '17:40:00', '19:20:00', '20:15:00', '21:10:00'];
+var classEndTimeJA = ['', '09:00:00', '09:55:00', '11:00:00', '11:55:00', '14:35:00', '15:30:00', '16:25:00', '17:30:00', '18:25:00', '20:05:00', '21:00:00', '21:55:00'];
+var events = [];
 var isInit = true;
+var tempName;
 function checkAuth() {
     gapi.auth.authorize(
         {
@@ -27,10 +31,7 @@ function handleAuthResult(authResult) {
         $('#authorize-div').css('display', 'none');
         $('#jwc_auth_form').css('display', 'inline');
         Materialize.toast("Google账户登录成功", 3000);
-        // loadCalendarApi();
     } else {
-        // Show auth UI, allowing the user to initiate authorization by
-        // clicking authorize button.
         $('#authorize-div').css('display', 'inline');
         if (!isInit) {
             Materialize.toast("Google账户登录失败", 3000);
@@ -39,7 +40,7 @@ function handleAuthResult(authResult) {
 }
 
 function handleGoogleAuthClick(event) {
-    isInit=false;
+    isInit = false;
     $('#progress_bar').css('display', 'inline');
     gapi.auth.authorize(
         {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
@@ -49,78 +50,90 @@ function handleGoogleAuthClick(event) {
 
 
 function loadCalendarApi() {
-    gapi.client.load('calendar', 'v3', listUpcomingEvents);
-}
-
-function startSync() {
+    gapi.client.load('calendar', 'v3', startSync);
     return false;
 }
-function listUpcomingEvents() {
-    var request = gapi.client.calendar.events.list({
-        'calendarId': 'primary',
-        'timeMin': (new Date()).toISOString(),
-        'showDeleted': false,
-        'singleEvents': true,
-        'maxResults': 100,
-        'orderBy': 'startTime'
-    });
-
-    // request.execute(function (resp) {
-    //     var events = resp.items;
-    //     appendPre('Upcoming events:');
-    //     if (events.length > 0) {
-    //         for (i = 0; i < events.length; i++) {
-    //             var event = events[i];
-    //             var when = event.start.dateTime;
-    //             if (!when) {
-    //                 when = event.start.date;
-    //             }
-    //             appendPre(event.summary + ' (' + when + ')')
-    //         }
-    //     } else {
-    //         appendPre('No upcoming events found.');
-    //     }
-    //
-    // });
-
-
-    var event = {
-        'summary': 'Google I/O 2015',
-        'location': '800 Howard St., San Francisco, CA 94103',
-        'description': 'A chance to hear more about Google\'s developer products.',
-        'start': {
-            'dateTime': '2017-02-01T09:00:00',
-            'timeZone': 'Asia/Shanghai'
-        },
-        'end': {
-            'dateTime': '2017-02-01T17:00:00',
-            'timeZone': 'Asia/Shanghai'
-        },
-        'recurrence': [
-            'RRULE:FREQ=DAILY;COUNT=2'
-        ],
-        'colorId': '11',
-        'attendees': [],
-        'reminders': {
-            'useDefault': false,
-            'overrides': [
-                {'method': 'popup', 'minutes': 30}
-            ]
-        }
-    };
-
-    request = gapi.client.calendar.events.insert({
+function haha() {
+    if (events.length == 0) {
+        Materialize.toast('导入完成', 3000);
+        $('#progress_bar').css('display', 'none');
+        $('#authorize-div').css('display', 'inline');
+        return false;
+    }
+    var event = events.shift();
+    tempName = event['summary'];
+    var request = gapi.client.calendar.events.insert({
         'calendarId': 'primary',
         'resource': event
     });
+    request.execute(function (event) {
+        Materialize.toast('导入' + tempName + '成功');
+        haha();
+        $('#progress_hint').text('正在导入' + tempName);
+    });
 
-    // request.execute(function (event) {
-    //     appendPre('Event created: ' + event.htmlLink);
-    // });
+}
+function startSync() {
+    $('#show_schedule_div').css('display', 'none');
+    $('#progress_bar').css('display', 'inline');
+    var arr = $('.filled-in');
+    for (var item in arr) {
+        if (arr[item].checked) {
+            for (var timeItem in mSchedule[item]['time']) {
+                if (mSchedule[item]['time'][timeItem]['start'] == null) {
+                    continue;
+                }
+                var startClass = mSchedule[item]['time'][timeItem]['start'];
+                var endClass = mSchedule[item]['time'][timeItem]['end'];
+                var week = mSchedule[item]['time'][timeItem]['week'];
+                var startDate = new Date();
+                startDate.setFullYear(2017, 1, 26);
+                var startClassDate = new Date();
+                startClassDate.setFullYear(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+                if (!(mSchedule[item]['even'] || mSchedule[item]['odd']))
+                    startClassDate.setDate(startClassDate.getDate() + week - 7 + 7 * mSchedule[item]['weekStart']);
+                else if (mSchedule[item]['even'])
+                    startClassDate.setDate(startClassDate.getDate() + week + 7);
+                else
+                    startClassDate.setDate(startClassDate.getDate() + week);
+                var count = mSchedule[item]['weekEnd'] - mSchedule[item]['weekStart'] + 1;
+                if (mSchedule[item]['even'] || mSchedule[item]['odd'])
+                    count = 18 / 2;
+                var startString = startClassDate.getFullYear() + '-' + (startClassDate.getMonth() + 1) + '-' + startClassDate.getDate();
+                var event = {
+                    'summary': mSchedule[item]['title'],
+                    'location': mSchedule[item]['time'][timeItem]['address'],
+                    'description': (mSchedule[item]['description'] + ' by SCUScheduler'),
+                    'start': {
+                        'dateTime': (startString + 'T' + ((mSchedule[item]['time'][timeItem]['address'].indexOf('江安') >= 0) ? classStartTimeJA[startClass] : classStartTimeWJ[startClass])),
+                        'timeZone': 'Asia/Shanghai'
+                    },
+                    'end': {
+                        'dateTime': (startString + 'T' + ((mSchedule[item]['time'][timeItem]['address'].indexOf('江安') >= 0) ? classEndTimeJA[endClass] : classEndTimeWJ[endClass])),
+                        'timeZone': 'Asia/Shanghai'
+                    },
+                    'recurrence': [
+                        ('RRULE:FREQ=WEEKLY;COUNT=' + count + ((mSchedule[item]['even'] || mSchedule[item]['odd']) ? ';INTERVAL=2' : ''))
+                    ],
+                    'colorId': (item % 11 + 1).toString(),
+                    'attendees': [],
+                    'reminders': {
+                        'useDefault': false,
+                        'overrides': [
+                            {'method': 'popup', 'minutes': 30}
+                        ]
+                    }
+                };
+                events.push(event);
+            }
+        }
+    }
+    haha();
+    $('#progress_hint').text('正在导入' + tempName);
 }
 
 function showSchedule(item, index) {
-    var str = '<li><div class="collapsible-header "><div class="row"><br><div class="col s11"><input type="checkbox" class="filled-in center-align" id="filled-in-box" checked="checked"/><label for="filled-in-box" class="black-text center-align">' +
+    var str = '<li><div class="collapsible-header "><div class="row"><br><div class="col s11"><input type="checkbox" class="filled-in center-align" id="check-' + index + '" checked="checked"/><label for="check-' + index + '" class="black-text center-align">' +
         item['title'] +
         '</label></div><div class="col white-text">.</div></div></div><div class="collapsible-body"><br><div class="row"><div class="col s12"><div class="col"><i class="material-icons">class</i></div><div class="col">';
     if (!(item['even'] || item['odd'])) {
@@ -144,7 +157,6 @@ function showSchedule(item, index) {
     str += '<div class="row"><div class="col s12"><div class="col"><i class="material-icons">assignment</i></div><div class="col">' +
         item['description'] +
         '</div></div></div></div></li>';
-
     $('#schedule_list').append(str);
 
 }
@@ -169,56 +181,3 @@ function getSchedule(form) {
         });
     return false;
 }
-
-var x = {
-    "kind": "calendar#colors",
-    "updated": "2012-02-14T00:00:00.000Z",
-    "calendar": {
-        "event": {
-            "1": {
-                "background": "#a4bdfc",
-                "foreground": "#1d1d1d"
-            },
-            "2": {
-                "background": "#7ae7bf",
-                "foreground": "#1d1d1d"
-            },
-            "3": {
-                "background": "#dbadff",
-                "foreground": "#1d1d1d"
-            },
-            "4": {
-                "background": "#ff887c",
-                "foreground": "#1d1d1d"
-            },
-            "5": {
-                "background": "#fbd75b",
-                "foreground": "#1d1d1d"
-            },
-            "6": {
-                "background": "#ffb878",
-                "foreground": "#1d1d1d"
-            },
-            "7": {
-                "background": "#46d6db",
-                "foreground": "#1d1d1d"
-            },
-            "8": {
-                "background": "#e1e1e1",
-                "foreground": "#1d1d1d"
-            },
-            "9": {
-                "background": "#5484ed",
-                "foreground": "#1d1d1d"
-            },
-            "10": {
-                "background": "#51b749",
-                "foreground": "#1d1d1d"
-            },
-            "11": {
-                "background": "#dc2127",
-                "foreground": "#1d1d1d"
-            }
-        }
-    }
-};
